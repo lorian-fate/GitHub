34373bb20803b3e5b8b72643bf1e222fa1bfe9d3
lorian-fate <ta01001011@gmail.com>
2021-04-10 13:01:33 +0200

    file updated on subpractice branch

--- practice_1.py
+++ practice_1.py
@@ -9,6 +9,9 @@ def count_B(x):
 def count_C(x):
     return x**(1/2)*x**2
 
+def count_D(x):
+    return [value for value in range(0, x, 2)]
+
 a = list(map(count_V, [29, 32, 16, 23, 87, 36]))
 
 print(a)
